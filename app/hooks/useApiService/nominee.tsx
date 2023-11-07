86d5003ea4bc8831a562bde889b453d46575c1b0
Elijah Ohiwerei <elijahohimairei@gmail.com>
2023-11-07 16:38:46 +0000

    create: crud action handlers hooks

---
+++ app/hooks/useApiService/nominee.tsx
@@ -0,0 +1,30 @@
+import { FetchNominees } from "@/app/services/fetchNominees";
+import { useQuery } from "@tanstack/react-query";
+
+export function useGetAdmin() {
+  const { data, isPending, isSuccess, isError, error } = useQuery({
+    queryKey: ["nominee"],
+    queryFn: async () => await new FetchNominees().getNominee(),
+  });
+
+  return { data, isPending, isSuccess, isError, error };
+}
+
+export function useGetAllNomination() {
+  const { data, isPending, isSuccess, isError, error } = useQuery({
+    queryKey: ["all-nominations"],
+    queryFn: async () => await new FetchNominees().getAllNominations(),
+  });
+
+  return { data, isPending, isSuccess, isError, error };
+}
+
+export function useGetUserNomination(nomination_id: string) {
+  const { data, isPending, isSuccess, isError, error } = useQuery({
+    queryKey: ["user-nominations"],
+    queryFn: async () =>
+      await new FetchNominees().getUserNomination(nomination_id),
+  });
+
+  return { data, isPending, isSuccess, isError, error };
+}
