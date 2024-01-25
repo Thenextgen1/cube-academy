2f42fd3b52e11e04b4af0ff29ecc8bcd3af2bcd9
Elijah Ohiwerei <elijahohimairei@gmail.com>
2024-01-25 12:09:59 +0000

    create: dummy data

---
+++ app/data/ratings.ts
@@ -0,0 +1,27 @@
+export const ratings = [
+  {
+    remark: "Very Unfair",
+    avatar: "/icons/very_unfair.svg",
+    value: (1 / 5) * 100,
+  },
+  {
+    remark: "Unfair",
+    avatar: "/icons/unfair.svg",
+    value: (2 / 5) * 100,
+  },
+  {
+    remark: "Not Sure",
+    avatar: "/icons/not_sure.svg",
+    value: (3 / 5) * 100,
+  },
+  {
+    remark: "Fair",
+    avatar: "/icons/fair.svg",
+    value: (4 / 5) * 100,
+  },
+  {
+    remark: "Very Fair",
+    avatar: "/icons/very_fair.svg",
+    value: (5 / 5) * 100,
+  },
+];
